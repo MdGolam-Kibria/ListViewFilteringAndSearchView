ee40ce0dc80f79a4af6405071e57da5b4a4fe3c2
Kibria92 <kibria92@gmail.com>
2020-02-22 05:50:51 +0600

    ListView Filtering And SearchView

---
+++ app/src/main/java/com/example/listviewfilteringandsearchview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.listviewfilteringandsearchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    SearchView searchView;
+    ListView listView;
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchView = (SearchView) findViewById(R.id.searchView);
+        listView = (ListView) findViewById(R.id.listView);
+        String name[] = getResources().getStringArray(R.array.item_name);
+        arrayAdapter = new ArrayAdapter(MainActivity.this, R.layout.simple, R.id.textSimple, name);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.search_view_action_bar, menu);
+        MenuItem menuItem = menu.findItem(R.id.kibria);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                arrayAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
